02bb9a5351c301cbd4c7f875fb291dc5da6a603a
Richie Bendall <richiebendall@gmail.com>
2020-02-22 00:15:42 +1300

    refactor: Make less awkward
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const commonDir = require('commondir');
 const pkgDir = require('pkg-dir');
 const makeDir = require('make-dir');
 
+const {env} = process;
+
 const isWritable = path => {
 	try {
 		fs.accessSync(path, fs.constants.W_OK);
@@ -14,8 +16,21 @@ const isWritable = path => {
 	}
 };
 
-function getNodeModuleDirectory(directory, name) {
+function useDirectory(directory, options) {
+	if (options.create) {
+		makeDir.sync(directory);
+	}
+
+	if (options.thunk) {
+		return (...arguments_) => path.join(directory, ...arguments_);
+	}
+
+	return directory;
+}
+
+function getNodeModuleDirectory(directory) {
 	const nodeModules = path.join(directory, 'node_modules');
+
 	if (
 		!isWritable(nodeModules) &&
 		(fs.existsSync(nodeModules) || !isWritable(path.join(directory)))
@@ -23,38 +38,30 @@ function getNodeModuleDirectory(directory, name) {
 		return undefined;
 	}
 
-	return path.join(directory, 'node_modules', '.cache', name);
+	return nodeModules;
 }
 
 module.exports = (options = {}) => {
-	const {name} = options;
-	const cacheDir = process.env.CACHE_DIR;
-	let directory = cacheDir ? path.join(cacheDir, 'find-cache-dir') : options.cwd || process.cwd();
-
-	if (!cacheDir) {
-		if (options.files) {
-			directory = commonDir(directory, options.files);
-		}
-
-		directory = pkgDir.sync(directory);
+	if (env.CACHE_DIR) {
+		return useDirectory(path.join(env.CACHE_DIR, 'find-cache-dir'), options);
 	}
+
+	let directory = options.cwd || process.cwd();
+
+	if (options.files) {
+		directory = commonDir(directory, options.files);
+	}
+
+	directory = pkgDir.sync(directory);
 
 	if (directory) {
-		if (!cacheDir) {
-			directory = getNodeModuleDirectory(directory, name);
-			if (directory === undefined) {
-				return undefined;
-			}
+		const nodeModules = getNodeModuleDirectory(directory);
+		if (!nodeModules) {
+			return undefined;
 		}
 
-		if (options.create) {
-			makeDir.sync(directory);
-		}
-
-		if (options.thunk) {
-			return (...arguments_) => path.join(directory, ...arguments_);
-		}
+		return useDirectory(path.join(directory, 'node_modules', '.cache', options.name), options);
 	}
 
-	return directory;
+	return undefined;
 };
